a9d2ad0d46d0254c48aaef5b313b7cfd9bd6bd1b
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2016-05-02 10:14:30 +0200

    Dz

---
+++ PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public enum TaktikaGađanja{
+        Napipavanje,
+        Okruživanje,
+        SustavnoUništavanje
+    }
+
+    public class Topništvo
+    {
+        public Topništvo()
+        {
+            PromjeniTaktikuUNapipavanje();
+        }
+
+        public Polje UputiPucanj()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGađanje(RezultatGađanja rezultat)
+        {
+            //DZ da testovi prođu
+            if (rezultat == RezultatGađanja.Potonuće)
+                PromjeniTaktikuUNapipavanje();
+            else 
+            if (rezultat == RezultatGađanja.Pogodak)
+            {
+                if (TrenutnaTaktika == TaktikaGađanja.Napipavanje)
+                    PromjeniTaktikuUOkruživanje();
+                else if (TrenutnaTaktika == TaktikaGađanja.Okruživanje)
+                    PromjeniTaktikuUSustavnoUništavanje();
+            }
+        }
+
+        private void PromjeniTaktikuUNapipavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Napipavanje;
+        }
+
+        private void PromjeniTaktikuUOkruživanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Okruživanje;
+        }
+
+        private void PromjeniTaktikuUSustavnoUništavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.SustavnoUništavanje;
+        }
+
+        public TaktikaGađanja TrenutnaTaktika
+        {
+            get;
+            private set;
+        }
+
+    }
+}
