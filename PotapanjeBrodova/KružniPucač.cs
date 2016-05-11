3c8c4d17d2e070cdf5aad5097c9e8b0bfe3decd0
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2016-05-11 20:50:06 +0200

    KružniPucač.cs -> UputiPucanj -> testovi rade

--- PotapanjeBrodova/KružniPucač.cs
+++ PotapanjeBrodova/KružniPucač.cs
@@ -16,12 +16,32 @@ namespace PotapanjeBrodova
             this.prvoPogođeno = prvoPogođeno;
         }
 
+        // tražimo za sve smjerove broj slobodnih polja u tom smijeru
+        // listu od 4 liste polja
+        // sortiraj te listem, tako da je najdulja prva
+        // između najduljih biram jednu slučanjim odabirom
+        // iz nje vadim prvo polje
         public Polje UputiPucanj()
         {
-            throw new NotImplementedException();
+            int redak = prvoPogođeno.Redak;
+            int stupac = prvoPogođeno.Stupac;
+            List<IEnumerable<Polje>> kandidati =new List<IEnumerable<Polje>>();
+
+            foreach (Smijer smjer in Enum.GetValues(typeof(Smijer)))
+            {
+                kandidati.Add(mreža.DajPoljaUZadanomSmjeru(redak, stupac, smjer));
+            }
+            kandidati.Sort((lista1, lista2) => lista2.Count() - lista1.Count());
+            var grupe=kandidati.GroupBy(lista => lista.Count());
+            var najdulji = grupe.First();
+            if (najdulji.Count() == 1)
+                return najdulji.First().First();
+            int indeks = slučajni.Next(najdulji.Count());
+             return najdulji.ElementAt(indeks).First();
         }
 
-        private Polje prvoPogođeno;
-        private Mreža mreža;
+        Polje prvoPogođeno;
+        Mreža mreža;
+        Random slučajni = new Random();
     }   
 }
